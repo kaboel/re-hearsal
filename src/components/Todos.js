1c6281535303ac678c3af8abaf58839639a8510e
Faiq <faiq.kaboel@gmail.com>
2019-06-23 06:20:25 +0700

    Add prop-types

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -16,7 +16,9 @@ class Todos extends React.Component {
 }
 
 Todos.propTypes = {
-    todos: PropTypes.array.isRequired
+    todos: PropTypes.array.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
 };
 
 export default Todos;
